9ec78132f091cafb428f83bc3f6655a30c2362b8
cuthere01 <ufa.timurah@gmail.com>
2024-10-16 17:15:06 +0300

    export test#2

--- pages/[type]/[alias].tsx
+++ pages/[type]/[alias].tsx
@@ -58,7 +58,8 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
     return {
         paths,
-        fallback: false,
+        fallback: "blocking",
+        //fallback: false,
         // fallback: true,
     };
 };
