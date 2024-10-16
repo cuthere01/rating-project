16b05b015db8e784b65b890e475cc6d408a16428
cuthere01 <ufa.timurah@gmail.com>
2024-10-16 16:20:40 +0300

    export test#1

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "export",
+    basePath: "/rating-project",
     reactStrictMode: true,
     images: {
-        domains: ['old-images.hb.ru-msk.vkcs.cloud']
+        domains: ["old-images.hb.ru-msk.vkcs.cloud"],
+        unoptimized: true,
     },
     webpack(config) {
         // Grab the existing rule that handles SVG imports
-        const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.(".svg"));
+        const fileLoaderRule = config.module.rules.find((rule) =>
+            rule.test?.test?.(".svg")
+        );
 
         config.module.rules.push(
             // Reapply the existing rule, but only for svg imports ending in ?url
@@ -19,9 +24,11 @@ const nextConfig = {
             {
                 test: /\.svg$/i,
                 issuer: fileLoaderRule.issuer,
-                resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+                resourceQuery: {
+                    not: [...fileLoaderRule.resourceQuery.not, /url/],
+                }, // exclude if *.svg?url
                 use: ["@svgr/webpack"],
-            },
+            }
         );
 
         // Modify the file loader rule to ignore *.svg, since we have it handled now.
@@ -29,7 +36,6 @@ const nextConfig = {
 
         return config;
     },
-    
 
     // ...other config
 };
